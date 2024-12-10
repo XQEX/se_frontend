753445af5725874e8fab9edf944a23c8cbf94136
Eagle <peejadeemak@gmail.com>
2024-12-10 15:29:39 +0700

    idk sidebar?

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="flex flex-col h-full overflow-y-auto">
+      <div className="flex justify-center items-center flex-col">
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+        <input
+          type="text"
+          placeholder="ค้นหาที่นี่"
+          className="rounded-lg border border-seagreen p-1 m-4 "
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/pages/Find.tsx
+++ src/pages/Find.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Searchbar from "../components/Searchbar";
+import Sidebar from "../components/Sidebar";
 
 function Find() {
   return (
-    <div className="h-screen ">
-      <Navbar />
-      <div className="flex flex-col justify-center items-center mt-4">
+    <div className="h-screen flex ">
+      <div className="w-1/4 bg-gray-100">
+        <Sidebar />
+      </div>
+      <div className="w-3/4">
+        <Navbar />
+        <div className="flex flex-col justify-center items-center mt-4"></div>
       </div>
     </div>
   );
