61a6fcf1714ae76d6f270fa408c222fbaafa0d03
Eagle <peejadeemak@gmail.com>
2024-12-10 10:31:37 +0700

    init react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-
-
-
+import { Route, Routes } from "react-router-dom";
+import Find from "./pages/Find";
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path = "find" element = {<Find/>} />
+        <Route path="signup" element={<SignUp/>} />
+        <Route path = "signin" element = {<SignIn/>} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
