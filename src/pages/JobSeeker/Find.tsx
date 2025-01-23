f777fb5213c8342157cd5df68d8d99fc1de5aebe
GodMonera <nongpalm.pp@gmail.com>
2025-01-24 01:03:29 +0700

    add profile page but not finished yet

--- src/pages/JobSeeker/Find.tsx
+++ src/pages/JobSeeker/Find.tsx
@@ -9,11 +9,9 @@ import { jobData } from "../../data/FakeJobData";
 import { ScrollArea } from "@mantine/core";
 import { motion } from "framer-motion";
 
-
 function Find() {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(9);
-
 
   const [favJobs, setFavJobs] = useState<string[]>([]);
 
@@ -40,24 +38,23 @@ function Find() {
         <div className="w-3/4 w-full">
           <div className="kanit-medium m-6 text-2xl">ค้นหางาน</div>
           <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-3 m-1">
-            
             {currentJobs.map((job, index) => (
-               <motion.div
-               key={index}
-               initial={{ opacity: 0, y: 20 }}
-               animate={{ opacity: 1, y: 0 }}
-               exit={{ opacity: 0, y: -20 }}
-               transition={{ duration: 0.5, ease: "easeOut" }}
-             >
-              <JobCard
-                id={job.id}
+              <motion.div
                 key={index}
-                title={job.title}
-                company={job.company}
-                time={job.time}
-                location={job.location}
-                salary={job.salary}
-              />
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+              >
+                <JobCard
+                  id={job.id}
+                  key={index}
+                  title={job.title}
+                  company={job.company}
+                  time={job.time}
+                  location={job.location}
+                  salary={job.salary}
+                />
               </motion.div>
             ))}
           </div>
@@ -77,6 +74,3 @@ function Find() {
 }
 
 export default Find;
-
-
-
