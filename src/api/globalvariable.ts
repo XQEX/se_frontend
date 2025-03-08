a525abb0fae3802d07491a751a31166453b565db
GodMonera <nongpalm.pp@gmail.com>
2025-03-08 22:40:25 +0700

    Update baseURL to use localhost for local development

--- src/api/globalvariable.ts
+++ src/api/globalvariable.ts
@@ -1,2 +1,4 @@
 export const backendPort = 6977;
-export const baseURL = `http://172.188.240.153`;
+// export const baseURL = `http://172.188.240.153`;
+
+export const baseURL = `http://localhost:6977`;
